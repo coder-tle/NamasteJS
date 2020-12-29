5f4857b6cda28d7840809af5aba336ff4617c589
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2020-12-30 01:43:14 +0530

    Practised callback and event loop concepts

---
+++ callback_practices/callback_practice.js
@@ -0,0 +1,7 @@
+console.log("start");
+
+setTimeout(function cb(){
+    console.log("Callback");
+}, 5000);
+
+console.log("end");
---
+++ callback_practices/click_event_callback.js
@@ -0,0 +1,8 @@
+console.log("Start");
+
+document.getElementById("btn").addEventListener("click",
+function cb(){
+    console.log("Callback");
+});
+
+console.log("End");

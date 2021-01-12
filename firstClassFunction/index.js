024127d01c17b2ce31c261d8de0e0fdc96608680
Sarfaraz Alam <sarfarazalam.sa53@gmail.com>
2021-01-12 23:51:31 +0530

    practised firs class function concept in javascript

---
+++ firstClassFunction/index.js
@@ -0,0 +1,16 @@
+
+
+//function statement aka function declaration
+function a(){
+    console.log("a called");
+}
+
+
+// function expression 
+var b = function (){
+    console.log("b called");
+}
+
+
+// difference between function statement & function expression 
+// lies in hoisting 
